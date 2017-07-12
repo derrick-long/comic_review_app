388db637b02ffc128f3abe34727adca799267c97
derrick-long <long.derrick8@gmail.com>
2017-07-11 21:05:16 -0400

    Trying to fix mailer production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
-host = 'https://infinite-peak-69788.herokuapp.com'
+host = 'infinite-peak-69788.herokuapp.com'
 config.action_mailer.default_url_options = { host: host }
 config.action_mailer.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
@@ -80,7 +80,7 @@ config.action_mailer.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'infinite-peak-69788.herokuapp.com',
+  :domain         =>  'heroku.com',
   :enable_starttls_auto => true
 }
 config.action_mailer.perform_caching = false
