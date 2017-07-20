f1b7596664b418614ba1d184246688058343fd9b
derrick-long <long.derrick8@gmail.com>
2017-07-20 17:41:42 -0400

    more mailer misery

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,12 +5,10 @@ class Review < ApplicationRecord
   validates :content, length: { in: 10..700 }, presence: true
   validates :rating, numericality: { only_integer: true },
             presence: true, inclusion: { in: 1..5 }
-#add dependent delete
+
 
   def vote_count
     self.votes.sum(:value)
   end
 
-  # so this should return the correct vote object,  which I can then use
-# to link to my edit and destroy functions
 end
